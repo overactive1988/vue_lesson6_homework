89f0ff6d30b678327aedb010e45118b8de011605
bhw-lee <t.lee@bhworks.com>
2021-06-27 19:44:31 +0900

    第五周前後台配置至vue cli

--- week06_1/src/router/index.js
+++ week06_1/src/router/index.js
@@ -1,20 +1,57 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    //404
+    path: "/:pathMatch(.*)*",
+    component: () => import("@/views/404.vue"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    //登入介面
+    path: "/login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    //前台介面
+    path: "/",
+    component: () => import("@/views/Index.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("@/views/Top.vue"),
+      },
+      {
+        path: "products",
+        component: () => import("@/views/Products.vue"),
+      },
+      {
+        path: "product/:id",
+        component: () => import("@/views/Product.vue"),
+      },
+      {
+        path: "cart",
+        component: () => import("@/views/Cart.vue"),
+      },
+    ],
+  },
+  {
+    //後台介面
+    path: "/admin",
+    component: () => import("@/views/admin/Admin.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("@/views/admin/AdminTop.vue"),
+      },
+      {
+        path: "products",
+        component: () => import("@/views/admin/AdminProducts.vue"),
+      },
+      {
+        path: "orders",
+        component: () => import("@/views/admin/AdminOrders.vue"),
+      },
+    ],
   },
 ];
 
